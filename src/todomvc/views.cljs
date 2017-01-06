6bc08c1b08011b1ce132c536791726358daa07ec
Chris Thompson <cthompson@cthompson-mbp.stp01.office.gdi>
2017-01-05 22:57:42 -0600

    add new fantastic form function

--- src/todomvc/views.cljs
+++ src/todomvc/views.cljs
@@ -31,40 +31,84 @@
 (defn input [form-type identifier attr & {:as html-attrs}]
   (field form-type identifier attr html-attrs))
 
+(defn calculate-command [form-type action]
+  (keyword (name form-type) (name action)))
+
+(defn save-fn
+  ([[form-type id]]
+   ;; call it, save all the inputs
+   (apply save-fn id {} {}))
+  ([[form-type id] args]
+   ;; in case you don't want to pass opts
+   (save-fn [form-type id] args {}))
+  ([[form-type id] args opts]
+   ;; opts can be options like :solo "don't merge values from :inputs"
+   (fn []
+     (let [new-args (if (fn? args) (args) args)
+           action (if (= :new id) :new :update)
+           calculated-command (calculate-command form-type action)]
+       (dispatch [:request/command calculated-command (merge {:id id} new-args) opts])))))
+
+(defn form
+  "returns function as closures around subscriptions to a single 'editable' thing in the
+  db. The thing has attributes, whose current value is accessed by calling `inputs' e.g., with arguments. No arguments will return all the attributes"
+  [form-type identifier]
+  (let [inputs-atom (subscribe [:editable form-type identifier :inputs])
+        state-atom (subscribe [:editable form-type identifier :state])
+        errors-atom (subscribe [:editable form-type identifier :errors])
+        defaults-atom (subscribe [:editable form-type identifier :defaults])
+        inputs (fn [& args] (get-in @inputs-atom args))
+        state (fn [& args] (get-in @state-atom args))
+        errors (fn [& args] (get-in @errors-atom args))
+        defaults (fn [& args] (get-in @defaults-atom args))]
+    {:inputs inputs
+     :state  state
+     :errors errors
+     :defaults defaults
+
+     :save (partial save-fn [form-type identifier])
+     :delete #(dispatch [:request/command (calculate-command form-type :delete) {:id identifier}])
+     :reset  #(dispatch (e/editable-reset :todos id (state :reset)))
+     :edit   #(dispatch [:editable
+                         [:editable :todos identifier :state :reset (inputs)]
+                         [:editable :todos identifier :state :editing true]])
+     }))
+
 (defn todo-item
   [todo]
   (let [id (get-in todo [:inputs :id])
-        inputs (subscribe [:editable :todos id :inputs])
-        done (subscribe [:editable :todos id :inputs :done])
-        label (subscribe [:editable :todos id :inputs :todo])
-        errors (subscribe [:editable :todos id :errors])
-        state (subscribe [:editable :todos id :state])
-        reset #(dispatch (e/editable-reset :todos id (:reset @state)))
-        incomplete #(dispatch [:editable :todos id :inputs :done false])
-        toggle #(dispatch [:editable :todos id :inputs :done (not %)])
-        save #(dispatch [:request/command :todos id {:command :todos/update}])
-        delete #(dispatch [:request/command :todos id {:command :todos/delete}])
-        edit #(dispatch [:editable
-                         [:editable :todos id :state :reset @inputs]
-                         [:editable :todos id :state :editing true]])]
+        {:keys [inputs
+                state
+                errors
+                save
+                delete
+                reset
+                edit]} (form :todos id)]
     (fn []
-      [:li {:class (str (when @done "completed ")
-                        (when (:editing @state) "editing"))}
+      [:li {:class (cond-> ""
+                     (inputs :done)   (str " completed")
+                     (state :pending) (str " pending")
+                     (state :editing) (str " editing"))}
        [:div.view
         [checkbox :todos id :done
          :class "toggle"
-         :on-change #(do (toggle @done) (save))]
+         ;; here we rely on the response from the server to update the form
+         ;; :solo means only send these attributes, and only update these
+         ;; attributes in the response handler
+         :on-change (save (fn [] {:done (not (inputs :done))}) {:solo true})]
         [:label
+         ;; edit will "toggle" :editing
          {:on-double-click edit}
-         @label]
+         (inputs :todo)]
         [:button.destroy
          {:on-click delete}]]
-       (when (:editing @state)
+       (when (state :editing)
+         ;; reset and save will "toggle" :editing
          [input :todos id :todo
           :id (str id "-id")
           :class "edit"
           :on-blur reset
-          :on-key-down (detect-controls {:enter #(do (incomplete) (save))
+          :on-key-down (detect-controls {:enter (save {:done false})
                                          :escape reset})])])))
 
 (defn task-list
@@ -104,11 +148,12 @@
 
 (defn task-entry []
   (let [new-todo (subscribe [:editable :todos :new])
-        stop #(dispatch (e/editable-reset :todos :new (:defaults @new-todo)))
-        ;; the defaults will need to get submitted along with the inputs, right?
-        ;; the response handler needs the defaults to reset the inputs to
-        save #(dispatch [:request/command :todos :new {:command :todos/new
-                                                       :tap {:defaults (:defaults @new-todo)}}])]
+        defaults (subscribe [:editable :todos :new :defaults])
+        stop #(dispatch (e/editable-reset :todos :new @defaults))
+        ;; the defaults will get merged into the inputs before sending
+        ;; also, the response handler needs the defaults to reset the inputs to
+        save #(dispatch [:request/command :todos/new :new {:tap {:defaults @defaults}
+                                                           :identifier :new}])]
     (fn []
       [input :todos :new :todo
        :id "new-todo"
@@ -156,7 +201,12 @@
           {:dispatch (e/editable-response form-type identifier response defaults)
            :db (assoc-in db [:editable form-type id :inputs] args)}
           "update"
-          {:dispatch (e/editable-response form-type identifier response args)}
+          (if (:solo tap)
+            ;; merge :args into :inputs
+            {:db(update-in db [:editable form-type id :inputs] merge args)
+             :dispatch [:editable form-type id :state :pending false]}
+            ;; reset :inputs to :args
+            {:dispatch (e/editable-response form-type identifier response args)})
           "delete"
           {:db (update-in db [:editable form-type] dissoc identifier)}
           )
