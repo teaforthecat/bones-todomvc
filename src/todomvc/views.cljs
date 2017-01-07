fb84e6f54fc29d0d070b967407a2703ed26e3390
Chris Thompson <cthompson@cthompson-mbp.stp01.office.gdi>
2017-01-06 20:00:15 -0600

    use e/form

--- src/todomvc/views.cljs
+++ src/todomvc/views.cljs
@@ -4,85 +4,6 @@
             [bones.editable :as e]
             [cljs.spec :as s]))
 
-(defn detect-controls [{:keys [enter escape]}]
-  (fn [keypress]
-    (case (.-which keypress)
-      13 (enter)
-      ;; chrome won't fire 27, so use on-blur instead
-      27 (escape)
-      nil)))
-
-(defn field [form-type identifier attr html-attrs]
-  (let [path [:editable form-type identifier :inputs attr]
-        value (subscribe path)
-        input-type (or (:input-type html-attrs) :input)
-        value-attr (or (:value-attr html-attrs) :value)
-        opts (dissoc html-attrs :value-attr :input-type)]
-    (fn []
-      [input-type (merge {:on-change #(dispatch-sync (conj path (-> % .-target .-value)))
-                          value-attr @value}
-                         opts)])))
-
-(defn checkbox [form-type identifier attr & {:as html-attrs}]
-  (field form-type identifier attr (merge {:type "checkbox"
-                                           :value-attr :checked}
-                                          html-attrs)))
-
-(defn input [form-type identifier attr & {:as html-attrs}]
-  (field form-type identifier attr html-attrs))
-
-(defn calculate-command [form-type action]
-  (keyword (name form-type) (name action)))
-
-(defn save-fn
-  ([[form-type id]]
-   ;; call it, save all the inputs. this way it can be used without parens, which
-   ;; is kind of neat
-   ((apply save-fn [form-type id] {} {})))
-  ([[form-type id] args]
-   ;; in case you don't want to pass opts
-   (save-fn [form-type id] args {}))
-  ([[form-type id] args opts]
-   ;; opts can be options like :solo "don't merge values from :inputs"
-   (fn []
-     (let [new-args (if (fn? args) (args) args)
-           action (if (= :new id) :new :update)
-           ;; args are attributes/inputs of the thing
-           more-args (merge (if (= :new id) nil {:id id}) new-args)
-           ;; opts are logistical things like how to update the form from the
-           ;; response
-           ;; we can't send :id equal to :new because :id is probably an
-           ;; attribute that will be generated
-           more-opts (merge (if (= :new id) {:identifier :new} ) opts)
-           ;; convention for commands here e.g.: :todos/update
-           calculated-command (calculate-command form-type action)]
-       (dispatch [:request/command calculated-command more-args more-opts])))))
-
-(defn form
-  "returns function as closures around subscriptions to a single 'editable' thing in the
-  db. The thing has attributes, whose current value is accessed by calling `inputs' e.g., with arguments. No arguments will return all the attributes"
-  [form-type identifier]
-  (let [inputs-atom (subscribe [:editable form-type identifier :inputs])
-        state-atom (subscribe [:editable form-type identifier :state])
-        errors-atom (subscribe [:editable form-type identifier :errors])
-        defaults-atom (subscribe [:editable form-type identifier :defaults])
-        inputs (fn [& args] (get-in @inputs-atom args))
-        state (fn [& args] (get-in @state-atom args))
-        errors (fn [& args] (get-in @errors-atom args))
-        defaults (fn [& args] (get-in @defaults-atom args))]
-    {:inputs inputs
-     :state  state
-     :errors errors
-     :defaults defaults
-
-     :save (partial save-fn [form-type identifier])
-     :delete #(dispatch [:request/command (calculate-command form-type :delete) {:id identifier}])
-     :reset  #(dispatch (e/editable-reset :todos identifier (state :reset)))
-     :edit   #(dispatch [:editable
-                         [:editable :todos identifier :state :reset (inputs)]
-                         [:editable :todos identifier :state :editing true]])
-     }))
-
 (defn todo-item
   [todo]
   (let [id (get-in todo [:inputs :id])
@@ -92,14 +13,14 @@
                 save
                 delete
                 reset
-                edit]} (form :todos id)]
+                edit]} (e/form :todos id)]
     (fn []
       [:li {:class (cond-> ""
                      (inputs :done)   (str " completed")
                      (state :pending) (str " pending")
                      (state :editing) (str " editing"))}
        [:div.view
-        [checkbox :todos id :done
+        [e/checkbox :todos id :done
          :class "toggle"
          ;; here we rely on the response from the server to update the form
          ;; :solo means only send these attributes, and only update these
@@ -113,12 +34,12 @@
          {:on-click delete}]]
        (when (state :editing)
          ;; reset and save will "toggle" :editing
-         [input :todos id :todo
+         [e/input :todos id :todo
           :id (str id "-id")
           :class "edit"
           :on-blur reset
-          :on-key-down (detect-controls {:enter (save {:done false})
-                                         :escape reset})])])))
+          :on-key-down (e/detect-controls {:enter (save {:done false})
+                                           :escape reset})])])))
 
 (defn task-list
   []
@@ -156,15 +77,15 @@
         "Clear completed"])]))
 
 (defn task-entry []
-  (let [{:keys [reset save]} (form :todos :new)]
+  (let [{:keys [reset save]} (e/form :todos :new)]
     (fn []
-      [input :todos :new :todo
+      [e/input :todos :new :todo
        :id "new-todo"
        :placeholder "What needs to be done?"
        :auto-focus true
        :on-blur reset
-       :on-key-down (detect-controls {:enter save
-                                      :escape reset})])))
+       :on-key-down (e/detect-controls {:enter save
+                                        :escape reset})])))
 
 (defn todo-app
   []
